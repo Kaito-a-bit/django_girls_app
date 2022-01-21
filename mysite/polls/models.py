679cb3f93fb1f0512866f8a8b1b449ba3941253d
Kaito-a-bit <arakai0128@gmail.com>
2022-01-22 00:56:05 +0900

    finished chapter3

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from pyexpat import model
+#from typing_extensions import Self
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    #ForeignKey defines relationship, which means one question is associated with the one question here.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
